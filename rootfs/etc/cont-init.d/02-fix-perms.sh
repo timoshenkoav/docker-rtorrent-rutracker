7fc40ccd4c5c18a6487db12e991a9e37c00faffb
Alexandr Timoshenko <thick.tav@gmail.com>
2021-03-28 22:48:04 +0200

    initial commit

---
+++ rootfs/etc/cont-init.d/02-fix-perms.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/with-contenv sh
+
+echo "Fixing perms..."
+mkdir -p /data/rtorrent \
+  /data/rutorrent \
+  /passwd \
+  /etc/nginx/conf.d \
+  /etc/rtorrent \
+  /var/cache/nginx \
+  /var/lib/nginx \
+  /var/run/nginx \
+  /var/run/php-fpm \
+  /var/run/rtorrent
+chown rtorrent. \
+  /data \
+  /data/rtorrent \
+  /data/rutorrent \
+  /downloads
+chown -R rtorrent. \
+  /etc/rtorrent \
+  /passwd \
+  /tpls \
+  /var/cache/nginx \
+  /var/lib/nginx \
+  /var/log/php7 \
+  /var/run/nginx \
+  /var/run/php-fpm \
+  /var/run/rtorrent
